91696a202d74246e3ecb7d7949af1fc9b36ca4bf
= <luis@up4tech.com.br>
2023-01-29 16:16:17 -0300

    carousel finalizado

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,46 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
 
-import hacker from '../../img/hacker.png';
-import securityData from '../../img/security-data.png';
-import company from '../../img/company.png';
+import slide1 from '../../img/slide1.png';
+import slide2 from '../../img/slide2.png';
+import slide3 from '../../img/slide3.png';
+import slide4 from '../../img/slide4.png';
 
 function Home(){
     return (
         <section className="w-full min-h-[200px] pt-[50px] flex flex-col justiy-center items-center">
             <h2 className="text-[#ffffff] text-4xl font-bold">Some <span className="text-[#ffff00f7]">news</span></h2>
-            <Carousel className="w-[90%] min-h-[200px] max-h-[300px] sm:max-w-[600px] sm:min-h-[400px] sm:max-h-[500px]">
+            <Carousel className="w-[90%] h-[200px] sm:w-[800px] sm:h-[300px]">
                 <Carousel.Item interval={1500}>
                     <img
-                        src={hacker}
-                        className="w-full h-[300px] sm:w-full sm:w-[400px] sm:h-[400px] bg-[#1f1f1f] rounded-md"
+                        src={slide1}
+                        className="w-full h-[150px] sm:h-[250px] bg-[#1f1f1f] border-t-[1px] border-b-[1px] border-t-white border-b-white"
                     />
-                    {/* <Carousel.Caption>
-                        <h3 className="text-[#0EA5E9] text-5xl font-bold">Security</h3>
-                        <p>Information security</p>
-                    </Carousel.Caption> */}
                 </Carousel.Item>
-
                 <Carousel.Item interval={1500}>
                     <img
-                        src={securityData}
-                        className="w-full h-[300px] sm:w-full sm:w-[400px] sm:h-[400px] bg-[#1f1f1f] rounded-md"
+                        src={slide2}
+                        className="w-full h-[150px] sm:h-[250px] bg-[#1f1f1f] border-t-[1px] border-b-[1px] border-t-white border-b-white"
                     />
-                    {/* <Carousel.Caption>
-                        <h3 className="text-[#0EA5E9] text-5xl font-bold">Security</h3>
-                        <p>Information security</p>
-                    </Carousel.Caption> */}
                 </Carousel.Item>
-
                 <Carousel.Item interval={1500}>
                     <img
-                        src={company}
-                        className="w-full h-[300px] sm:w-full sm:w-[400px] sm:h-[400px] bg-[#1f1f1f] rounded-md"
+                        src={slide3}
+                        className="w-full h-[150px] sm:h-[250px] bg-[#1f1f1f] border-t-[1px] border-b-[1px] border-t-white border-b-white"
                     />
-                    {/* <Carousel.Caption>
-                        <h3 className="text-[#0EA5E9] text-5xl font-bold">Security</h3>
-                        <p>Information security</p>
-                    </Carousel.Caption> */}
+                </Carousel.Item>
+                <Carousel.Item interval={1500}>
+                    <img
+                        src={slide4}
+                        className="w-full h-[150px] sm:h-[250px] bg-[#1f1f1f] border-t-[1px] border-b-[1px] border-t-white border-b-white"
+                    />
                 </Carousel.Item>
             </Carousel>
 
