096fadea3025ee164d8c81b3a80e4d3705cd7ee5
= <luis@up4tech.com.br>
2023-01-28 22:37:41 -0300

    ocultando senha do formulário

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,17 @@
-import { BsPersonCircle, BsKeyFill, BsEyeFill } from 'react-icons/bs';
+import { useState } from 'react';
+import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
 import team from '../../img/virtual.png'
 
 function Login(){
+    const [ showPassword, setShowPassword ] = useState(false);
+
+    // Responsible for the passoword:
+    const handleShowPassord = () => setShowPassword(!showPassword);
+
     return(
-        <section className="w-full h-screen  bg-[#1f1f1f] sm:bg-[#1f1f1f] flex flex-row sm:flex-row-reverse justify-center items-center sm:justify-start sm:rounded-none">
-            <section className="w-full sm:w-[50%] sm:h-screen bg-[#242424] rounded-md sm:rounded-none p-[20px] sm:flex sm:flex-col sm:justify-center sm:items-center">
+        <section className="w-full  bg-[#1f1f1f] sm:bg-[#1f1f1f] flex flex-col-reverse sm:flex-row-reverse justify-center items-center sm:justify-start sm:rounded-none">
+            <section className="w-full min-h-[600px] sm:w-[50%] sm:h-screen bg-[#242424] rounded-md sm:rounded-none p-[20px] sm:flex sm:flex-col sm:justify-center sm:items-center">
                 <div className="w-full sm:w-[80%] min-h-[300px] pb-[10px] sm:min-h-[50%] flex flex-col justify-center items-center">
                     <h3 className="w-[80%] text-[#fffafa] font-bold text-3xl">Hey, <span className="text-[#ffff00f7]">hello</span></h3>
                     <span className="w-[80%] text-[#C0C0C0] text-sm">Enter the information you entered while registering</span>
@@ -17,16 +23,17 @@ function Login(){
                         <div className="w-full h-[60px] sm:max-w-[400px] border-b-2 border-gray-400 hover:border-sky-500 duration-100 mt-[15px]">
                             <label className="text-[#fafafa] text-lg">Password:</label>
                             <div className="w-full mt-[3px] flex flex-row">
-                                <input type="text" className="w-full bg-[#242424] outline-0 text-[#0EA5E9]" placeholder="enter your password" />
-                                <BsEyeFill style={{color: "#fafafa", fontSize: 25, cursor: "pointer"}} />
+                                <input type={showPassword ? "text" : "password"} className="w-full bg-[#242424] outline-0 text-[#0EA5E9]" placeholder="enter your password" />
+                                <button onClick={handleShowPassord}>
+                                    {showPassword ? <BsEyeFill style={styles.password} /> : <BsEyeSlashFill style={styles.password} />}
+                                </button>
                             </div>
-                            
                         </div>
                         <div className="w-full sm:max-w-[400px] mt-[10px]">
                             <div className="w-full h-[60px] flex flex-col sm:flex-row sm:justify-between">
                                 <div className="max-w-[50%] h-[30px] flex flex-row items-center">
                                     <input type="checkbox" name="rememberMe" value="rememberMe" />
-                                    <label for="rememberMe" className="ml-[3px] text-[#C0C0C0] hover:text-[#0EA5E9] text-sm cursor-pointer">Remember me</label>
+                                    <label className="ml-[3px] text-[#C0C0C0] hover:text-[#0EA5E9] text-sm cursor-pointer">Remember me</label>
                                 </div>
                                 <div className="max-w-[50%] h-[30px] flex flex-row items-center cursor-pointer">
                                     <a className="text-[#C0C0C0] hover:text-[#0EA5E9] text-sm">Forgot password?</a>
@@ -45,12 +52,16 @@ function Login(){
                 </div>
             </section>
 
-            <section className="hidden sm:block sm:w-[50%] sm:h-screen sm:flex sm:justify-center sm:items-center">
-                {/* <Lottie options={welcomeOptions} width={300} height={300}/> */}
+            <section className="w-full h-[400px] sm:w-[50%] sm:h-screen sm:flex sm:justify-center sm:items-center">
                 <img src={team} className="w-[450px]" />
             </section>            
         </section>
     )
+}
+
+
+const styles = {
+    password:{color: "#fafafa", fontSize: 25 }
 }
 
 export default Login;
